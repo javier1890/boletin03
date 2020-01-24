31323449c02cfcc67b08670a98cb63f67c805476
javier1890 <60121488+javier1890@users.noreply.github.com>
2020-01-24 10:32:14 -0300

    subiendo tareas boletin03

---
+++ Seguimiento01.java
@@ -0,0 +1,19 @@
+package boletin03;
+
+public class Seguimiento01 {
+
+	public static void main(String[] args) {
+		// 6. Calcular el valor de la posición fibonacci usando recursividad.
+				int suma;
+			    for (int i = 1; i < 4; i++) {
+					for (int j = 3; j >0; j--) {
+						suma=i*10+j;
+						
+						System.out.println("La suma es:"+suma);
+					}
+				 }
+				}
+
+	}
+
+
---
+++ Seguimiento02.java
@@ -0,0 +1,20 @@
+package boletin03;
+
+
+public class Seguimiento02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int suma;
+		int j=0;
+	    for (int i = 1; i < 3; i++) {
+			j=i+1;
+			while (j<4) {
+				System.out.println(j-4);
+				j=j+1;
+				
+			}
+		 }
+		}
+	}
+
